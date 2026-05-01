b3d1ccf4abcf5d0797bcb1506f10f5778506555a
sangij24001-admin <san-j24001@sangi.jp>
2026-05-01 20:22:27 +0900

    管理者アカウントを挿入する処理をシーダーに追加

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class AdminUserSeeder extends Seeder
 {
@@ -12,6 +16,14 @@ class AdminUserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $password = 'admin';
+        $hashed_pass = Hash::make($password);
+
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin01@example.com',
+            'password' => $hashed_pass,
+            'role' => 'admin',
+        ]);
     }
 }
