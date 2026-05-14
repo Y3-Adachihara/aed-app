f8f37abe6b2c3a2bcd06649b325fd86cdbfe675f
Your Name <san-j24001@sangi.jp>
2026-05-14 09:44:03 +0900

    top_titleに文字列を追加

--- resources/views/aed/index.blade.php
+++ resources/views/aed/index.blade.php
@@ -1,5 +1,5 @@
 <x-layout.layout>
-    <x-layout.layout-header view_name="AEDアプリ | ホーム" top_title="">
-        
+    <x-layout.layout-header view_name="AEDアプリ | ホーム" top_title="島田市 AED設置場所一覧">
+
     </x-layout.layout-header>
 </x-layout.layout>
