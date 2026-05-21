b3f26a034a7741911328a785752ea0e812632318
Your Name <san-j24001@sangi.jp>
2026-05-21 16:18:01 +0900

    シーダーの挿入許可リストを設定

--- app/Models/Aed.php
+++ app/Models/Aed.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+#[Fillable(['name', 'postcode', 'prefecture', 'municipality', 'address', 'description', 'latitude', 'longitude'])]
 class Aed extends Model
 {
     //
