a9a55bf5638214951077d940158c6a31a192a72e
Your Name <san-j24001@sangi.jp>
2026-05-22 10:18:52 +0900

    データを一覧表示するための仮コードを追加

--- resources/views/aed/index.blade.php
+++ resources/views/aed/index.blade.php
@@ -1,5 +1,19 @@
+@props([
+    'aeds' => [],
+])
 <x-layout.layout>
     <x-layout.layout-header view_name="AEDアプリ | ホーム" top_title="島田市 AED設置場所一覧">
-
+        @foreach ($aeds as $aed)
+            <br/>
+            <p>{{ $aed->name }}</p>
+            <p>{{ $aed->postcode }}</p>
+            <p>{{ $aed->prefecture }}</p>
+            <p>{{ $aed->municipality }}</p>
+            <p>{{ $aed->address }}</p>
+            <p>{{ $aed->description }}</p>
+            <p>{{ $aed->latitude }}</p>
+            <p>{{ $aed->longitude }}</p>
+            <br/>
+        @endforeach
     </x-layout.layout-header>
 </x-layout.layout>
