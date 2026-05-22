ae75fcb9482ae38943f6ef5d4698c6f4babfd214
Your Name <san-j24001@sangi.jp>
2026-05-22 14:53:35 +0900

    aedをnullで初期化するように変更し、カード部品のタグの書き方を修正

--- resources/views/aed/index.blade.php
+++ resources/views/aed/index.blade.php
@@ -1,19 +1,10 @@
 @props([
-    'aeds' => [],
+    'aeds' => null,
 ])
 <x-layout.layout>
     <x-layout.layout-header view_name="AEDアプリ | ホーム" top_title="島田市 AED設置場所一覧">
         @foreach ($aeds as $aed)
-            <br/>
-            <p>{{ $aed->name }}</p>
-            <p>{{ $aed->postcode }}</p>
-            <p>{{ $aed->prefecture }}</p>
-            <p>{{ $aed->municipality }}</p>
-            <p>{{ $aed->address }}</p>
-            <p>{{ $aed->description }}</p>
-            <p>{{ $aed->latitude }}</p>
-            <p>{{ $aed->longitude }}</p>
-            <br/>
+            <x-elements.aed.card :aed="$aed" />
         @endforeach
     </x-layout.layout-header>
 </x-layout.layout>
