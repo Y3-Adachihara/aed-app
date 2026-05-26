b1790ac30d34ac7b124a212282bb2216ed2367ee
Your Name <san-j24001@sangi.jp>
2026-05-26 15:13:17 +0900

    GoogleMAPを入れ込むコンポーネントを生成

---
+++ resources/views/components/elements/aed/map.blade.php
@@ -0,0 +1,7 @@
+@proos([
+    'latitude' => null,
+    'longitude' => null,
+])
+<div id='map' class="w-full h-96 bg-gray-200 rounded-xl flex items-center justify-center text-gray-500 mb-4">
+    // ここに地図
+</div>
