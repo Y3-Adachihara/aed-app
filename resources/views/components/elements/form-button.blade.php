8bebe5ef27183de00e4b8b9e437768be78ace2c4
Your Name <san-j24001@sangi.jp>
2026-06-04 08:55:21 +0900

    ボタンテーマがdefaultだった時のバグを修正＆コメントの仕方を修正

--- resources/views/components/elements/form-button.blade.php
+++ resources/views/components/elements/form-button.blade.php
@@ -3,19 +3,19 @@
     'method' => null,
     'title' => null,
     'confirm_message' => null,
-    'theme' => default,
+    'theme' => 'default',
 ])
 @php
     $themeClass = match($theme) {
         'edit' => 'text-amber-600 hover:bg-amber-50',
         'delete' => 'text-red-600 hover:bg-red-50',
         'default' => 'text-white bg-blue-600 hover:bg-blue-700 shadow-sm',
-    }
+    };
     $baseClass = 'px-3 py-1.5 text-sm font-medium rounded-md transition-colors';
 @endphp
 
 <form action="{{ $action }}" method="POST" onsubmit="return confirm('{{ $confirm_message }}')">
-    @method($method)    // method()の中は、自動的にphpと解釈される
+    @method($method)    {{-- method()の中は、自動的にphpと解釈される --}}
     @csrf
     
     <button type="submit" class="{{ $baseClass }} {{ $themeClass }}">
