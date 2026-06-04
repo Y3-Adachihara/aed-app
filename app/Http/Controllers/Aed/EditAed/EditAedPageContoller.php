4334a169cb489c96c874f33e4dc0415bbae13661
Your Name <san-j24001@sangi.jp>
2026-06-04 14:29:56 +0900

    Aedモデルのuse文を追加 & return文を従来通り修正

--- app/Http/Controllers/Aed/EditAed/EditAedPageContoller.php
+++ app/Http/Controllers/Aed/EditAed/EditAedPageContoller.php
@@ -4,17 +4,17 @@ namespace App\Http\Controllers\Aed\EditAed;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Model\Aed;
 
 class EditAedPageContoller extends Controller
 {
     public function __invoke(Request $request, $aedId)
     {
         $aed = Aed::where('id', $aedId)->firstOrFail();
-        // $top_title = $aed->name;
+        $top_title = $aed->name;
 
         $view_name = "AEDアプリ | 詳細";
 
-        // return view('aed.details', compact('view_name', 'top_title', 'aed'));
-        return view('aed.details', compact('top_title', 'aed'));
+        return view('aed.edit', compact('view_name', 'top_title', 'aed'));
     }
 }
