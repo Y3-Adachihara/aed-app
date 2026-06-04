e319749f2e3bf612d6a65cfd4e813d6e90601b06
Your Name <san-j24001@sangi.jp>
2026-06-04 15:10:42 +0900

    コントローラの綴り修正後のページ表示コントローラ

---
+++ app/Http/Controllers/Aed/EditAed/EditAedPageController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Aed\EditAed;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Aed;
+
+class EditAedPageContoller extends Controller
+{
+    public function __invoke(Request $request, $aedId)
+    {
+        $aed = Aed::where('id', $aedId)->firstOrFail();
+        $top_title = $aed->name;
+
+        $view_name = "AEDアプリ | 詳細";
+
+        return view('aed.edit', compact('view_name', 'top_title', 'aed'));
+    }
+}
