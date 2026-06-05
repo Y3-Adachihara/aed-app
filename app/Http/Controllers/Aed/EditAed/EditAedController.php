9566b05a63e9eecce18a9fdc6323923b021d62b3
Your Name <san-j24001@sangi.jp>
2026-06-05 14:48:19 +0900

    クラス名の誤り（PageのControllerとの重複）を修正

--- app/Http/Controllers/Aed/EditAed/EditAedController.php
+++ app/Http/Controllers/Aed/EditAed/EditAedController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Aed\EditRequest;
 use App\Models\Aed;
 
-class EditAedPageController extends Controller
+class EditAedController extends Controller
 {
     public function __invoke(EditRequest $request)
     {
