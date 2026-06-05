b10e661925cb8978186474001258ba17f5e325ea
Your Name <san-j24001@sangi.jp>
2026-06-05 12:32:39 +0900

    EditAedPageControllerのリファクタリングとリクエスト処理の改善(誤ってEditAedPageControllerに処理を記述していた)

--- app/Http/Controllers/Aed/EditAed/EditAedController.php
+++ app/Http/Controllers/Aed/EditAed/EditAedController.php
@@ -3,16 +3,27 @@
 namespace App\Http\Controllers\Aed\EditAed;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\Aed\EditRequest;
+use App\Models\Aed;
 
-class EditAedContoller extends Controller
+class EditAedPageController extends Controller
 {
-    public function __invoke(Request $request, $aedId)
+    public function __invoke(EditRequest $request)
     {
-        /*
-        $aed = Aed::where('id', $aedId)->firstOrFail();
-        $aed->delete();
+        $aed = Aed::where('id', $request->aedId())->firstOrFail();
+
+        $aed->name = $request->name();
+        $aed->postcode = $request->postcode();
+        $aed->prefecture = $request->prefecture();
+        $aed->municipality = $request->municipality();
+        $aed->address = $request->address();
+        $aed->description = $request->description();
+
+        // 少数第四位まで。第五位以降は切り捨て。
+        $aed->latitude = floor($request->latitude() * 10000) / 10000;
+        $aed->longitude = floor($request->longitude() * 10000) / 10000;
+        
+        $aed->save();
         return redirect()->route('home');
-        */
     }
 }
