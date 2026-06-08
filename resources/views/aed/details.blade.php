5a88f44658c7c938f6e55fd8accbeef94a7781ad
Your Name <san-j24001@sangi.jp>
2026-06-08 14:11:52 +0900

    詳細ページのレイアウトを改善し、情報を整理して表示するように変更

--- resources/views/aed/details.blade.php
+++ resources/views/aed/details.blade.php
@@ -5,13 +5,18 @@
 ])
 <x-layout.layout>
     <x-layout.layout-header view_name="{{ $view_name }}" top_title="{{ $top_title }}">
-        <p>{{ $aed->prefecture }}{{ $aed->municipality }}{{ $aed->address }}</p>
-        <p>{{ $aed->description }}</p>
-        <p>{{ $aed->latitude }}</p>
-        <p>{{ $aed->longitude }}</p>
+            <div class="space-y-4 bg-white p-6 rounded-xl shadow-sm border border-gray-100 mt-4">
+            <p class="text-lg font-medium text-gray-800">{{ $aed->prefecture }}{{ $aed->municipality }}{{ $aed->address }}</p>
+            <p class="text-gray-600">{{ $aed->description }}</p>
+            <p class="text-sm text-gray-400">緯度: {{ $aed->latitude }} / 経度: {{ $aed->longitude }}</p>
 
-        <x-elements.aed.map :latitude="$aed->latitude" :longitude ="$aed->longitude" />
+            <x-elements.aed.map :latitude="$aed->latitude" :longitude="$aed->longitude" />
 
-        <a href="{{ route('home') }}">戻る</a>
+            <div class="pt-4">
+                <a href="{{ route('home') }}" class="inline-flex items-center text-sm font-medium text-blue-600 hover:text-blue-800">
+                    ← 戻る
+                </a>
+            </div>
+        </div>
     </x-layout.layout-header>
 </x-layout.layout>
