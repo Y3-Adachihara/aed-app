e85efb4c695ed761b5aa2d62096b370e803335bb
Your Name <san-j24001@sangi.jp>
2026-06-25 12:20:05 +0900

    CsvMappingSeederに各自治体のdataset_idを追加

--- database/seeders/CsvMappingSeeder.php
+++ database/seeders/CsvMappingSeeder.php
@@ -18,6 +18,7 @@ class CsvMappingSeeder extends Seeder
             [
                 'municipality_code' => '222097', // 島田市
                 'municipality_name' => '島田市',
+                'dataset_id' => '8f9aacc5-4c06-4803-acbe-5452b98b6f9c',
                 'name_column' => '名称',
                 'latitude_column' => '緯度',
                 'longitude_column' => '経度',
@@ -29,6 +30,7 @@ class CsvMappingSeeder extends Seeder
             [
                 'municipality_code' => '222127', // 焼津市
                 'municipality_name' => '焼津市',
+                'dataset_id' => 'e5f81371-ca06-4f6e-af7b-0dd1e853494f',
                 'name_column' => '施設名',
                 'latitude_column' => '緯度',
                 'longitude_column' => '経度',
@@ -40,6 +42,7 @@ class CsvMappingSeeder extends Seeder
             [
                 'municipality_code' => '221007', // 静岡市
                 'municipality_name' => '静岡市',
+                'dataset_id' => 'dd1347a8-60e9-4781-8dbe-7614924f945e',
                 'name_column' => '名称',
                 
                 // 💡 罠対策：データが空なので null を指定し、APIでの取得対象にする！
@@ -56,6 +59,7 @@ class CsvMappingSeeder extends Seeder
             [
                 'municipality_code' => '224294', // 川根本町
                 'municipality_name' => '川根本町',
+                'dataset_id' => 'e504fef5-2762-41da-ad8d-08b93c7f7ae7',
                 'name_column' => '名称',
                 'latitude_column' => null,  // 💡 緯度経度の列がないので null を許可
                 'longitude_column' => null, // 💡 経度の列がないので null を許可
